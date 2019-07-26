be304cc25b73f573f36fa3ba530eefb91d69e1ed
MyDuan <dy1226130575@163.com>
2019-07-26 12:40:20 +0900

    Make job suceessed and notify slack.

--- dags/hello_areflow.py
+++ dags/hello_areflow.py
@@ -2,17 +2,39 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
+from airflow.contrib.hooks.slack_webhook_hook import SlackWebhookHook
+
+def notify_error(context):
+
+    SlackWebhookHook(
+      http_conn_id='slack_playing_ml',
+      channel='ml',
+      username='airflow',
+      icon_emoji=':cat:',
+      message= '*Hello, world!*'#''' Task: {task_id} failed '''.format(task_id=context.get('task_instance').task_id)
+    ).execute()
+
+def notify_success(context):
+
+  SlackWebhookHook(
+      http_conn_id='slack_playing_ml',
+      channel='ml',
+      username='airflow',
+      icon_emoji=':dog:',
+      message= '*Hello, world!*' #''' Task: {task_id} successed '''.format(task_id=context.get('task_instance').task_id)
+  ).execute()
 
 # DAGを作る
 default_args = {
     'owner': 'ohke',
     'depends_on_past': False,
-    # 最初のタスクの実行日時は、start_dateではなく、start_date+schedule_intervalとなる
-    # イメージとしては、start_dateで指定した日時からデータが蓄積され始め、schedule_intervalが経過したらdailyであれば1日分のデータを処理を行う感じ
     'start_date': datetime(2019, 7, 24, 10, 0, 0), # DAGの実行開始日時
     'schedule_interval': timedelta(days=1), # 実行間隔
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
+    'provide_context': True,
+    'on_success_callback': notify_success,
+    'on_failure_callback': notify_error,
 }
 
 # DAGのID指定
@@ -29,7 +51,7 @@ t1 = BashOperator(
 
 t2 = BashOperator(
     task_id='t2',
-    bash_command='exit 1',
+    bash_command='exit 0',
     retries=3,
     dag=dag)
 
